2dff016c13aaec9828793c5f016f87413ac5b728
Wafaa.Alhayek <wafash.alhayek@gmail.com>
2023-01-22 17:54:40 +0200

    done

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    const newCats =[...cats];
+    newCats.push(name);
+    return newCats
+}
+
+function prependCat(name) {
+    const newCats = [...cats];
+    newCats.unshift(name);
+    return newCats
+}
+
+function removeLastCat() {
+    const newCats= [...cats];
+    newCats.pop();
+    return newCats
+}
+
+function removeFirstCat(){
+    const newCats = [...cats];
+    newCats.shift();
+    return newCats;
+}
